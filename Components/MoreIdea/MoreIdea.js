e1dfe15d7bd6a5669db48533891fdd67f43d50f3
Ibrahim <ibrahimcoderx@gmail.com>
2023-12-16 22:11:57 +0600

    Update site link

--- Components/MoreIdea/MoreIdea.js
+++ Components/MoreIdea/MoreIdea.js
@@ -14,16 +14,16 @@ const MoreIdea = () => {
         <img src="/images/home/img.png" alt="" />
 
         <ul>
-            <li> <Link href=""> <span>1.</span>Louisiana</Link></li>
-            <li> <Link href=""> <span>2.</span>Bahamas</Link></li>
-            <li> <Link href=""> <span>3.</span>Jamaica</Link></li>
-            <li> <Link href=""> <span>4.</span>Florida</Link></li>
-            <li> <Link href=""> <span>5.</span>California</Link></li>
-            <li> <Link href=""> <span>6.</span>Maryland</Link></li>
-            <li> <Link href=""> <span>7.</span>Fiji</Link></li>
-            <li> <Link href=""> <span>8.</span>Weekend Getaways</Link></li>
-            <li> <Link href=""> <span>9.</span>Luxury Vacation</Link></li>
-            <li> <Link href=""> <span>10.</span>Beaches United states</Link></li>
+            <li> <Link href="louisiana"> <span>1.</span>Louisiana</Link></li>
+            <li> <Link href="bahamas"> <span>2.</span>Bahamas</Link></li>
+            <li> <Link href="jamaica"> <span>3.</span>Jamaica</Link></li>
+            <li> <Link href="florida"> <span>4.</span>Florida</Link></li>
+            <li> <Link href="california"> <span>5.</span>California</Link></li>
+            <li> <Link href="maryland"> <span>6.</span>Maryland</Link></li>
+            <li> <Link href="fiji"> <span>7.</span>Fiji</Link></li>
+            <li> <Link href="weekend-getaways"> <span>8.</span>Weekend Getaways</Link></li>
+            <li> <Link href="luxury-vacatio"> <span>9.</span>Luxury Vacation</Link></li>
+            <li> <Link href="beaches-us"> <span>10.</span>Beaches United states</Link></li>
 
         </ul>
 
